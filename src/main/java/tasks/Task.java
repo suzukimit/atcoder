029d3574a723bacccf5c0658bc137e6733132c0b
Mitsuru Suzuki <mtr.suzuki@gmail.com>
2022-04-30 01:00:00 +0900

    Git Push Agent by crontab

--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -1,66 +1,12 @@
 package tasks;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Task {
     HashMap<Integer, Integer> callCount = new HashMap<>();
 
     public void solve(int testNumber, Scanner in, PrintWriter out) {
-        String seed = "";
-        int n = 0;
-        try {
-            seed = in.next();
-            n = in.nextInt();
-        } catch (Exception e) {
-            out.println("error!");
-            System.exit(1);
-        }
-        long ans = calc(n);
-        for (Map.Entry<Integer, Integer> entry: callCount.entrySet()) {
-            ans += call(seed, entry.getKey()) * entry.getValue();
-        }
-        out.println(calc(n));
-    }
-
-    private long calc(int n) {
-        if (n == 0) {
-            return 1;
-        } else if (n == 2) {
-            return 2;
-        } else if (n % 2 == 0) {
-            return calc(n - 1)  + calc(n - 2) - calc(n - 3) - calc(n - 4);
-        } else {
-            if (callCount.containsKey(n)) {
-                callCount.put(n, callCount.get(n) + 1);
-            } else {
-                callCount.put(n, 1);
-            }
-            return 0;
-        }
-    }
-
-    private long call(String seed, int n) {
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-
-            //TODO クエリパラメータを無理やり埋め込む
-            HttpRequest req = HttpRequest.newBuilder()
-                    .uri(URI.create("http://challenge-server.code-check.io/api/recursive/ask?seed=" + seed + "&n=" + n))
-                    .build();
-            HttpResponse res = client.send(req, HttpResponse.BodyHandlers.ofString());
-
-            //TODO jsonをパースしてrsultを返す
-            return 100;
-        } catch (IOException | InterruptedException e) {
-            return 0;
-        }
     }
 }
